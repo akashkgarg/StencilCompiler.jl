d4d7b37304751bdb8532d353a295f95c50a17e01
Akash Garg <git@akashgarg.org>
2021-07-29 14:11:49 -0400

    parallel stencil cpu with cartesian indices

--- examples/parallel_3pt_1d.jl
+++ examples/parallel_3pt_1d.jl
@@ -3,42 +3,6 @@
 
 using TiledIteration
 
-function iterate(dst::Array{T,1}, src::Array{T,1}, alpha::T, beta::T) where T
-    tilesize = Threads.nthreads() - 1
-    indices = TileIterator(axes(src), RelaxLastTile((tilesize,)))
-    for i in indices
-        for j in first(i)
-            if j == 1 || j == length(src)
-                dst[j] = src[j]
-            else
-                dst[j] = alpha*src[j] + beta*(dst[j-1] + dst[j+1])
-            end
-        end
-    end
-end
-
-function compute(t::Int, N::Int)
-    src_idx = Ref(1)
-    dst_idx = Ref(2)
-    data = [rand(N), zeros(N)]
-    for t in 1:t
-        iterate(data[dst_idx[]], data[src_idx[]], 1/3, 1/3)
-
-        # swap
-        tmp = dst_idx[]
-        dst_idx[] = src_idx[]
-        src_idx[] = tmp[]
-    end
-    return data[dst_idx[]]
-end
-
-
-# 3pt parallel stencil with time slicing with redundant computation on T timesteps. Barrier synchronziation
-# happens at hte end of T timesteps until final desired T timesteps is reached.
-
-stencil = [CartesianIndex(-1), CartesianIndex(0), CartesianIndex(1)]
-coeffs = [1/3, 1/3, 1/3]
-
 function stencilfunc!(dst, src, coord, stencil, coeffs)
     lower_bound = first(CartesianIndices(src))
     upper_bound = last(CartesianIndices(src))
@@ -56,15 +20,24 @@ function stencilfunc!(dst, src, coord, stencil, coeffs)
     return nothing
 end
 
+function iterate(dst::Array{T,1}, src::Array{T,1}, stencil, coeffs::Array{T,1}) where T
+    tilesize = Threads.nthreads() - 1
+    indices = TileIterator(axes(src), RelaxLastTile((tilesize,)))
+    #Threads.@threads for I in CartesianIndices(src)
+    for I in CartesianIndices(src)
+        stencilfunc!(dst, src, I, stencil, coeffs)
+    end
+end
+
 function compute(t::Int, N::Int)
     src_idx = Ref(1)
     dst_idx = Ref(2)
     data = [rand(N), zeros(N)]
-    println(data[src_idx[]])
+    stencil = [CartesianIndex(-1), CartesianIndex(0), CartesianIndex(1)]
+    coeffs = [1/3, 1/3, 1/3]
+
     for t in 1:t
-        for I in CartesianIndices(data[src_idx[]])
-            stencilfunc!(data[dst_idx[]], data[src_idx[]], I, stencil, coeffs)
-        end
+        iterate(data[dst_idx[]], data[src_idx[]], stencil, coeffs)
         # swap
         tmp = dst_idx[]
         dst_idx[] = src_idx[]
@@ -72,3 +45,6 @@ function compute(t::Int, N::Int)
     end
     return data[dst_idx[]]
 end
+
+# 3pt parallel stencil with time slicing with redundant computation on T timesteps. Barrier synchronziation
+# happens at hte end of T timesteps until final desired T timesteps is reached.
