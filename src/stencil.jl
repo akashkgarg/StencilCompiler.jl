7702e039395827ecaa63d8348aafa5ad0d26e887
Akash Garg <git@akashgarg.org>
2021-08-10 18:14:59 -0400

    adding stencil struct. parse index expression into number

--- src/stencil.jl
+++ src/stencil.jl
@@ -1,12 +1,17 @@
 import MacroTools: flatten, isexpr
 
-struct StencilRules
+struct Stencil{T}
+    coords::CartesianIndices
+    coeffs::Array{T, 1}
+end
+
+struct StencilRules{T}
     grid::Grid
 
-    boundaries::Dict{UnitRange, Function}
+    boundaries::Dict{UnitRange, Stencil{T}}
 
     # a stencil is is a tuple of index offsets and corresponding coefficients.
-    stencil::Tuple{CartesianIndices, Array{Float64, 1}}
+    stencil::Stencil{T}
 end
 
 macro stencil(expr)
@@ -25,6 +30,23 @@ function get_grid_symbol(exprs)
     return nothing
 end
 
+# computes and returns the index for an expression like i+1, replacing i with 0
+function get_index_from_symbolic_ref(expr)
+    if expr isa Symbol
+        return 0
+    elseif expr isa Number
+        return expr
+    elseif isexpr(expr) && expr.head == :call
+        op = expr.args[1]
+        lhs = get_index_from_symbolic_ref(expr.args[2])
+        rhs = get_index_from_symbolic_ref(expr.args[3])
+        return eval(Expr(:call, op, lhs, rhs))
+    end
+end
+
+function get_stencils(expr)
+end
+
 function __stencil(expr)
     rule_exprs = filter(isexpr, flatten(expr).args)
     grid_sym = get_grid_symbol(rule_exprs)
@@ -33,7 +55,7 @@ function __stencil(expr)
             println("lhs = $(expr.args[1]), rhs = $(expr.args[2])")
         end
     end
-    return StencilRules(grid_sym, {}, {})
+    return StencilRules{Float64}(grid_sym)
 end
 
 
